6c19baa110028021a8301e999fb9aaf0639eb6ba
adolphlwq <kenan3015@gmail.com>
2017-08-12 19:28:31 +0800

    add new main entiypoint

--- main.go
+++ main.go
@@ -2,31 +2,28 @@ package main
 
 import (
 	"flag"
+	"net"
 
-	"github.com/Sirupsen/logrus"
-)
-
-var (
-	usi     *URLServiceImpl
-	dbname  string
-	user    string
-	pass    string
-	address string
-	dbport  string
-	port    string
+	"github.com/adolphlwq/tinyurl/mysql"
+	"github.com/adolphlwq/tinyurl/server"
 )
 
 func main() {
-	flag.StringVar(&dbname, "dbname", "tinyurl", "database name to connect")
-	flag.StringVar(&user, "user", "test", "user of database")
-	flag.StringVar(&pass, "pass", "test", "pass of database")
-	flag.StringVar(&address, "address", "localhost", "address of database")
-	flag.StringVar(&dbport, "dbport", "3306", "port of database")
-	flag.StringVar(&port, "port", "8877", "port tinyurl bind on")
+	var (
+		configPath string
+		host       string
+		port       string
+	)
+	flag.StringVar(&configPath, "config", "default.properties", "config path")
+	flag.StringVar(&host, "host", "0.0.0.0", "tinyurl server bind host")
+	flag.StringVar(&port, "port", "8877", "tinyurl server bind port")
 	flag.Parse()
 
-	logrus.Info("Start init DB")
-	dbs := NewDB(dbname, user, pass, address, dbport)
-	usi = NewURLServiceImpl(dbs)
-	tinyUrlAPI(":" + port)
+	mysqlClient := mysql.NewMySQLClient(configPath)
+	sp := &server.ServiceProvider{
+		MysqlClient: mysqlClient,
+	}
+	addr := net.JoinHostPort(host, port)
+
+	server.Start(addr, sp)
 }
