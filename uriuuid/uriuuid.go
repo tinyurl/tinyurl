1975e06885b298f289a75e1020f7d646bcb38056
adolphlwq <kenan3015@gmail.com>
2017-08-18 19:53:16 +0800

    add DefaultLen and DefaultChars

--- uriuuid/uriuuid.go
+++ uriuuid/uriuuid.go
@@ -1,5 +1,12 @@
 package uriuuid
 
+var (
+	// DefaultChars chars to consists of random string
+	DefaultChars = []byte("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
+	// DefaultLen default random string length
+	DefaultLen = 6
+)
+
 // UriUUID interface for extension
 type UriUUID interface {
 	New() string
