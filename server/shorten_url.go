22e5519ee47fea4bf3f6f85da6ac6fe1b2ad7336
adolphlwq <kenan3015@gmail.com>
2017-08-19 17:17:59 +0800

    OriginUrl -> OriginURL

--- server/shorten_url.go
+++ server/shorten_url.go
@@ -11,21 +11,21 @@ import (
 
 // ShortenURL shorten origin url and save to db
 func ShortenURL(c *gin.Context, appService *ServiceProvider) {
-	originUrl := c.PostForm("origin_url")
+	OriginURL := c.PostForm("origin_url")
 
-	if originUrl == "" {
+	if OriginURL == "" {
 		c.JSON(http.StatusBadRequest, gin.H{
 			"message": "please provide origin_url.",
 		})
 		return
 	}
 	// check longurl
-	logrus.Infof("check if origin %s has existed in db.\n", originUrl)
+	logrus.Infof("check if origin %s has existed in db.\n", OriginURL)
 	var url entity.URL
-	appService.MysqlClient.DB.Where("origin_url = ?", originUrl).First(&url)
-	if url.OriginUrl == "" {
+	appService.MysqlClient.DB.Where("origin_url = ?", OriginURL).First(&url)
+	if url.OriginURL == "" {
 		url.CreateTime = time.Now().UTC()
-		url.OriginUrl = originUrl
+		url.OriginURL = OriginURL
 		url.ShortPath = "not implement"
 		c.JSON(http.StatusOK, gin.H{
 			"short_path": url.ShortPath,
