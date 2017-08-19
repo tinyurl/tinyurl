a5b8f5b555ee68d3591cdb3ae3cf26f37687dd92
adolphlwq <kenan3015@gmail.com>
2017-08-19 19:25:25 +0800

    add some test helper funcs

--- server/api_test.go
+++ server/api_test.go
@@ -1,16 +1,25 @@
 package server
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/url"
 	"testing"
 	"time"
 
+	"github.com/Sirupsen/logrus"
+	"github.com/adolphlwq/tinyurl/entity"
 	"github.com/adolphlwq/tinyurl/mysql"
+	"github.com/adolphlwq/tinyurl/uriuuid"
 )
 
 const (
-	TestPort   = "9090"
-	TestAddr   = "http://0.0.0.0:9090"
-	ConfigPath = "../test.properties"
+	TestPort      = "9090"
+	TestAddr      = "http://0.0.0.0:9090"
+	ConfigPath    = "../test.properties"
+	TestOriginURL = "http://test.origin.url"
+	TestShortPath = "shortpath"
 )
 
 var (
@@ -22,7 +31,48 @@ func init() {
 	mysqlClient = mysql.NewMySQLClient(ConfigPath)
 	appService = &ServiceProvider{
 		MysqlClient: mysqlClient,
+		UriUUID:     uriuuid.BasicURIUUID{},
 	}
+}
+
+func newTestURL() entity.URL {
+	return entity.URL{
+		OriginURL: TestOriginURL,
+		ShortPath: TestShortPath,
+	}
+}
+
+func insertTestURL(url entity.URL) {
+	appService.MysqlClient.DB.Create(&url)
+}
+
+func updateTestURL(url entity.URL) {
+	appService.MysqlClient.DB.Save(&url)
+}
+
+func clearDatabase() {
+	appService.MysqlClient.DropDatabase()
+}
+
+func PostForm(postURL string, data url.Values) interface{} {
+	resp, err := http.PostForm(postURL, data)
+	if err != nil {
+		logrus.Fatalf("post form data to %s error: %v", postURL, err)
+	}
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		logrus.Fatalf("read response body error: %v", err)
+	}
+
+	var ret interface{}
+	err = json.Unmarshal(body, &ret)
+	if err != nil {
+		logrus.Fatalf("unmarshal response body error: %v", err)
+	}
+
+	return ret
 }
 
 // startTestServer
