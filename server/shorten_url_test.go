43a7d30229294a23cefcb2a5702cc5f8596266ad
adolphlwq <kenan3015@gmail.com>
2017-08-19 19:26:57 +0800

    shorten url test case

---
+++ server/shorten_url_test.go
@@ -0,0 +1,35 @@
+package server
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShortenURL(t *testing.T) {
+	startTestServer(t)
+	shortenURLAPI := TestAddr + "/api/v1/shorten"
+
+	// test origin not existed in db
+	postData1 := url.Values{}
+	postData1.Set("origin_url", TestOriginURL)
+	ret1 := PostForm(shortenURLAPI, postData1)
+	map1 := ret1.(map[string]interface{})
+	assert.NotNil(t, map1)
+	shortPath := map1["short_path"].(string)
+	assert.Equal(t, len(shortPath), 6)
+	assert.Equal(t, map1["message"], "shorten url success.")
+
+	// test url has existed
+	testURL := newTestURL()
+	testURL.ShortPath = shortPath
+	updateTestURL(testURL)
+	ret2 := PostForm(shortenURLAPI, postData1)
+	map2 := ret2.(map[string]interface{})
+	assert.NotNil(t, map2)
+	assert.Equal(t, map2["message"], "short path has existed.")
+	assert.Equal(t, map2["short_path"], shortPath)
+
+	clearDatabase()
+}
