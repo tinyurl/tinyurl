6fca1d524aa201d768a0fc541358839dfd82bc87
adolphlwq <kenan3015@gmail.com>
2017-08-19 19:26:40 +0800

    integrate uriuuid

--- server/shorten_url.go
+++ server/shorten_url.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/adolphlwq/tinyurl/entity"
+	"github.com/adolphlwq/tinyurl/mysql"
 	"github.com/gin-gonic/gin"
 )
 
@@ -19,20 +20,41 @@ func ShortenURL(c *gin.Context, appService *ServiceProvider) {
 		})
 		return
 	}
+
 	// check longurl
 	logrus.Infof("check if origin %s has existed in db.\n", OriginURL)
 	var url entity.URL
 	appService.MysqlClient.DB.Where("origin_url = ?", OriginURL).First(&url)
+	// url.OriginURL == "" OriginURL does not exist in db, generate shortpath
 	if url.OriginURL == "" {
+		var retry = 0
+		shortPath := appService.UriUUID.New()
+		for isShortPathExisted(appService.MysqlClient, shortPath) {
+			shortPath = appService.UriUUID.New()
+			retry++
+		}
+		logrus.Infof("generate short path for %s with %d times\n", OriginURL, retry)
+
 		url.CreateTime = time.Now().UTC()
 		url.OriginURL = OriginURL
-		url.ShortPath = "not implement"
+		url.ShortPath = shortPath
+		appService.MysqlClient.DB.Create(&url)
+
 		c.JSON(http.StatusOK, gin.H{
+			"message":    "shorten url success.",
 			"short_path": url.ShortPath,
 		})
 	} else {
 		c.JSON(http.StatusOK, gin.H{
+			"message":    "short path has existed.",
 			"short_path": url.ShortPath,
 		})
 	}
+}
+
+// isShortPathExisted true short path exists or false not exist
+func isShortPathExisted(client *mysql.Client, shortPath string) bool {
+	var url entity.URL
+	client.DB.Where("short_path = ?", shortPath).First(&url)
+	return url.OriginURL != ""
 }
