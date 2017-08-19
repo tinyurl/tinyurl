400c103b92ee53c2f008880d3dec0dc956a1a048
adolphlwq <kenan3015@gmail.com>
2017-08-19 19:25:41 +0800

    add uriuuid instance

--- server/service_provider.go
+++ server/service_provider.go
@@ -1,8 +1,12 @@
 package server
 
-import "github.com/adolphlwq/tinyurl/mysql"
+import (
+	"github.com/adolphlwq/tinyurl/mysql"
+	"github.com/adolphlwq/tinyurl/uriuuid"
+)
 
 // ServiceProvider object hold service which server need
 type ServiceProvider struct {
 	MysqlClient *mysql.Client
+	UriUUID     uriuuid.UriUUID
 }
